febb5a5f20c9900102a6320e189b10428b04931e
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-10-03 18:06:25 +0600

    Finish with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import './App.css'
+import React, { useState } from 'react';
+import UserForm from './components/UserForm/UserForm.tsx';
+import Users from './components/Users/Users.tsx';
 
-function App() {
+interface User {
+  name: string;
+  email: string;
+  active: boolean;
+  role: string;
+}
 
+const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (user: User) => {
+    setUsers([...users, user]);
+  };
 
   return (
-    <>
-
-    </>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <UserForm addUser={addUser} />
+        </div>
+        <div className="col-md-6">
+          <Users users={users} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App
